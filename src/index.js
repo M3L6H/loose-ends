42ae279167cd81f2f3e80289332716ca304216e2
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-10-09 17:11:25 -0500

    feat: start clone

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-alert("Hello world");
+async function clone(repo, pat='') {
+  const uploadPackUrl = `${repo}/info/refs?service=git-upload-pack`;
+
+  const response = await fetch(uploadPackUrl, {
+    headers: {
+      'Git-Protocol': 'version=2',
+    },
+  });
+    
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+
+  const result = await response.text();
+
+  return result;
+}
+
+clone('https://github.com/M3L6H/loose-ends.git').then(refs => {
+  const textNode = document.createTextNode(refs);
+  document.body.appendChild(textNode);
+});
