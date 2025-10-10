404ac991198d6c54738a645a66c072a02ca784f3
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-10-10 06:47:07 -0500

    feat: list refs

--- src/index.js
+++ src/index.js
@@ -51,24 +51,95 @@ function proxyUrl(url) {
   return `https://corsproxy.io?url=${encodeURIComponent(url)}`;
 }
 
-async function clone(repo, pat='') {
-  const uploadPackUrl = proxyUrl(`${repo}/info/refs?service=git-upload-pack`);
+function toBytes(str) {
+  return Array.from(str).map(c => c.codePointAt(0));
+}
 
-  const response = await fetch(uploadPackUrl, {
+function lenToBytes(len) {
+  return toBytes(len.toString(HEX_BYTES).padStart(PKT_SIZE_BYTE_COUNT, '0'));
+}
+
+function createPktLines(lines) {
+  const msgLen = lines.reduce((acc, curr) =>
+    acc + 4 + curr.length + (curr || 1),
+  0);
+  
+  const data = new Uint8Array(msgLen);
+  let idx = 0;
+  
+  for (let line of lines) {
+    if (line === '') {
+      data.set(idx, [0, 0, 0, 0]);
+      idx += 4;
+      continue;
+    }
+    
+    if (!line.endsWith('\n')) {
+      line += '\n';
+    }
+    
+    const len = line.length;
+    data.set(idx, lenToBytes(len));
+    idx += 4;
+    
+    data.set(idx, toBytes(line));
+    idx += len;
+  }
+  
+  return data;
+}
+
+async function gitReq(url, method='GET', headers={}, body) {
+  const response = await fetch(url, {
     headers: {
       'User-Agent': 'git/2.0.0',
       'Accept': '*/*',
       'Git-Protocol': 'version=2',
+      ...headers,
     },
+    method,
+    body,
   });
-
+  
   if (!response.ok) {
     throw new Error(`Response status: ${response.status}`);
   }
+  
+  return await parsePktLines(response.body.getReader());
+}
 
-  const result = await parsePktLines(response.body.getReader());
-
+async function getCaps(repo, pat) {
+  const capUrl = proxyUrl(`${repo}/info/refs?service=git-upload-pack`);
+  const result = await gitReq(capUrl);
   return result;
+}
+
+async function lsRefs(repo, pat) {
+  const refsUrl = proxyUrl(`${repo}/git-upload-pack`);
+  const result = await gitReq(
+    refsUrl,
+    'POST',
+    { 
+      'Content-Type': 'application/x-git-upload-pack-request',
+    },
+    createPktLines([
+      'command=ls-refs',
+      'peel',
+      'symrefs',
+      'ref-prefix refs/heads/',
+      'ref-prefix refs/tags/',
+      '',
+    ]),
+  );
+  
+  return result;
+}
+
+async function clone(repo, pat='') {
+  const capabilities = getCaps(repo, pat);
+  console.log('Capabilities:', capabilities);
+
+  return await lsRefs(repo, pat);
 }
 
 function init() {
