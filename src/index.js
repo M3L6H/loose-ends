73d64ca389575e6c83ca185e772834157d3edc3e
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-10-09 22:26:36 -0500

    fix: use reader to iterate stream

--- src/index.js
+++ src/index.js
@@ -3,37 +3,44 @@ const PKT_SIZE_BYTE_COUNT = 4;
 
 const td = new TextDecoder('utf-8');
 
-async function parsePktLines(stream) {
+async function parsePktLines(reader) {
   const lines = [];
   let len = null;
   let s = e = 0;
   const curr = new Uint8Array(PKT_SIZE_BYTE_COUNT + 0xffff);
   
-  for await (const chunk of stream) {
-    curr.set(chunk, e);
-    e += chunk.length;
+  while (true) {
+    const { done, value } = await reader.read();
+
+    if (done) break;
+
+    for (let i = 0; i < value.length; i += curr.length - e) {
+      const chunk = value.subarray(i, curr.length - e);
+      curr.set(chunk, e);
+      e += chunk.length;
     
-    if (len === null) {
-      if (e - s < PKT_SIZE_BYTE_COUNT) continue;
-      const d = curr.subarray(s, s + PKT_SIZE_BYTE_COUNT);
-      s += PKT_SIZE_BYTE_COUNT;
-      len = parseInt(td.decode(d), HEX_BASE);
+      if (len === null) {
+        if (e - s < PKT_SIZE_BYTE_COUNT) continue;
+        const d = curr.subarray(s, s + PKT_SIZE_BYTE_COUNT);
+        s += PKT_SIZE_BYTE_COUNT;
+        len = parseInt(td.decode(d), HEX_BASE);
+      }
+
+      if (len > 0) {
+        if (e - s < len) continue;
+    
+        const d = curr.subarray(s, s + len);
+        s += len;
+        lines.push(td.decode(d).trim());
+      }
+
+      // Flush
+      const d = curr.subarray(s, e);
+      curr.set(d, 0);
+      e -= s;
+      s = 0;
+      len = null;
     }
-    
-    if (len > 0) {
-      if (e - s < len) continue;
-    
-      const d = curr.subarray(s, s + len);
-      s += len;
-      lines.push(td.decode(d).trim());
-    }
-    
-    // Flush
-    const d = curr.subarray(s, e);
-    curr.set(d, 0);
-    e -= s;
-    s = 0;
-    len = null;
   }
 
   return lines;
@@ -58,7 +65,7 @@ async function clone(repo, pat='') {
     throw new Error(`Response status: ${response.status}`);
   }
 
-  const result = await parsePktLines(response.body);
+  const result = await parsePktLines(response.body.getReader());
 
   return result;
 }
