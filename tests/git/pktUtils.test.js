288b87269ac27fa3e27cb11d176e4487c24f548a
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-10-14 14:34:08 -0500

    test: add test for parsing single line

--- tests/git/pktUtils.test.js
+++ tests/git/pktUtils.test.js
@@ -2,15 +2,31 @@ import { parsePktLines } from '@src/git/pktUtils';
 
 describe('pktUtils', () => {
   describe('parsePktLines', () => {
-    it('should return empty if the reader is empty', async () => {
-      const stream = new ReadableStream({
-        start(controller) {
-          // Close stream immediately
-          controller.close();
-        },
-      });
+    let data = [];
+    const stream = new ReadableStream({
+      start(ctrl) {
+        data.forEach(ctrl.enqueue);
+        ctrl.close();
+      },
+    });
 
+    beforeEach(() => {
+      data = [];
+    });
+
+    it('should return empty if the reader is empty', async () => {
       await expect(parsePktLines(stream.getReader())).resolves.toBeEmpty();
+    });
+    
+    it('should read a line', async () => {
+      data = [
+        // 0 0 0 4 t e s t
+        Uint8Array.fromHex('3030303474657374'),
+      ];
+    
+      await expect(parsePktLines(stream.getReader())).resolves.toEqual([
+        'test',
+      ]);
     });
   });
 });
