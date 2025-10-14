2b4cb0837f7ef80cc45cc4af36c6931677ce1803
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-10-14 14:50:48 -0500

    test: refresh stream between tests

--- tests/git/pktUtils.test.js
+++ tests/git/pktUtils.test.js
@@ -2,28 +2,28 @@ import { parsePktLines } from '@src/git/pktUtils';
 
 describe('pktUtils', () => {
   describe('parsePktLines', () => {
-    let data = [];
-    const stream = new ReadableStream({
-      start(ctrl) {
-        data.forEach(ctrl.enqueue);
-        ctrl.close();
-      },
-    });
+    let data, stream;
 
     beforeEach(() => {
       data = [];
+      stream = new ReadableStream({
+        start(ctrl) {
+          data.forEach(ctrl.enqueue);
+          ctrl.close();
+        },
+      });
     });
 
     it('should return empty if the reader is empty', async () => {
       await expect(parsePktLines(stream.getReader())).resolves.toBeEmpty();
     });
-    
+
     it('should read a line', async () => {
       data = [
         // 0 0 0 4 t e s t
         Uint8Array.fromHex('3030303474657374'),
       ];
-    
+
       await expect(parsePktLines(stream.getReader())).resolves.toEqual([
         'test',
       ]);
