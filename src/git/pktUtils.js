f46e6b5473a3b42e44fea7379165aee1a1aa4d64
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-10-14 16:54:21 -0500

    chore: remove debug logs

--- src/git/pktUtils.js
+++ src/git/pktUtils.js
@@ -19,8 +19,6 @@ export async function parsePktLines(reader) {
   while (true) {
     const { done, value } = await reader.read();
     
-    console.log(`value: ${value}`);
-
     if (done) break;
 
     for (let i = 0; i < value.length;) {
@@ -30,23 +28,18 @@ export async function parsePktLines(reader) {
       i += chunk.length;
 
       if (len === null) {
-        console.log('parsing len');
         if (e - s < PKT_SIZE_BYTE_COUNT) continue;
         const d = curr.subarray(s, s + PKT_SIZE_BYTE_COUNT);
         s += PKT_SIZE_BYTE_COUNT;
-        console.log(`decoded: ${td.decode(d)}`);
         len = parseInt(td.decode(d), HEX_BASE) - PKT_SIZE_BYTE_COUNT;
-        console.log(`len: ${len}`);
       }
 
       if (len > 0) {
-        console.log('parsing data');
         if (e - s < len) continue;
     
         const d = curr.subarray(s, s + len);
         s += len;
         lines.push(td.decode(d).trim());
-        console.log(`line: ${lines[lines.length - 1]}`);
       }
 
       // Flush
@@ -55,11 +48,8 @@ export async function parsePktLines(reader) {
       e -= s;
       s = 0;
       len = null;
-      console.log(`flush. curr: ${curr}`);
     }
   }
-  
-  console.log('done');
 
   return lines;
 }
