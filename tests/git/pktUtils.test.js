ed0cdd9c1436460a0c9fa041f64903e0869e1b58
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-10-14 15:01:09 -0500

    test: move setup into individual tests

--- tests/git/pktUtils.test.js
+++ tests/git/pktUtils.test.js
@@ -2,27 +2,27 @@ import { parsePktLines } from '@src/git/pktUtils';
 
 describe('pktUtils', () => {
   describe('parsePktLines', () => {
-    let data, stream;
+    it('should return empty if the reader is empty', async () => {
+      const stream = new ReadableStream({
+        start(ctrl) {
+          ctrl.close();
+        },
+      });
 
-    beforeEach(() => {
-      data = [];
-      stream = new ReadableStream({
+      await expect(parsePktLines(stream.getReader())).resolves.toBeEmpty();
+    });
+
+    it('should read a line', async () => {
+      const data = [
+        // 0 0 0 4 t e s t
+        Uint8Array.fromHex('3030303474657374'),
+      ];
+      const stream = new ReadableStream({
         start(ctrl) {
           data.forEach(ctrl.enqueue);
           ctrl.close();
         },
       });
-    });
-
-    it('should return empty if the reader is empty', async () => {
-      await expect(parsePktLines(stream.getReader())).resolves.toBeEmpty();
-    });
-
-    it('should read a line', async () => {
-      data = [
-        // 0 0 0 4 t e s t
-        Uint8Array.fromHex('3030303474657374'),
-      ];
 
       await expect(parsePktLines(stream.getReader())).resolves.toEqual([
         'test',
