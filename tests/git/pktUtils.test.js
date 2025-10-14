66b7355d4ea7f48b915f0e756806198bb4042d9a
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-10-14 15:04:13 -0500

    test: use lambda in setup

--- tests/git/pktUtils.test.js
+++ tests/git/pktUtils.test.js
@@ -2,13 +2,19 @@ import { parsePktLines } from '@src/git/pktUtils';
 
 describe('pktUtils', () => {
   describe('parsePktLines', () => {
-    it('should return empty if the reader is empty', async () => {
-      const stream = new ReadableStream({
+    let data, stream;
+    
+    beforeEach(() => {
+      data = [];
+      stream = new ReadableStream({
         start(ctrl) {
+          data.forEach(d => ctrl.enqueue(d));
           ctrl.close();
         },
       });
+    });
 
+    it('should return empty if the reader is empty', async () => {
       await expect(parsePktLines(stream.getReader())).resolves.toBeEmpty();
     });
 
@@ -17,12 +23,6 @@ describe('pktUtils', () => {
         // 0 0 0 4 t e s t
         Uint8Array.fromHex('3030303474657374'),
       ];
-      const stream = new ReadableStream({
-        start(ctrl) {
-          data.forEach(ctrl.enqueue);
-          ctrl.close();
-        },
-      });
 
       await expect(parsePktLines(stream.getReader())).resolves.toEqual([
         'test',
