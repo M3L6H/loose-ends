b875d027f8345b1a365777684268f15c2f17ee31
m3l6h <8094643+M3L6H@users.noreply.github.com>
2026-08-17 15:26:20 -0500

    feat: add canvas

--- src/git/index.js
+++
@@ -1,1 +0,0 @@
-export * as pktUtils from './pktUtils.js';
---
+++ src/scripts/canvas.js
@@ -0,0 +1,21 @@
+let canvas;
+let content;
+
+function resizeCanvas() {
+  canvas.height = content.clientHeight;
+  canvas.width = content.clientWidth;
+
+  drawContent();
+}
+
+function drawContent() {
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#3498db";
+  ctx.fillRect(20, 20, canvas.width - 40, canvas.height - 40);
+}
+
+export function init() {
+  canvas = document.getElementById("canvas");
+  content = document.getElementById("content");
+  window.addEventListener("resize", resizeCanvas());
+}
