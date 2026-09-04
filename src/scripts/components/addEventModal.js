c4368982f7a0921cc533f8ec913be2a5151368df
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-09-03 20:11:29 -0500

    feat: jump cursor to end of timestamp

--- src/scripts/components/addEventModal.js
+++ src/scripts/components/addEventModal.js
@@ -12,4 +12,12 @@ export function init() {
     dateInput.placeholder = now;
     dateInput.value = now;
   });
+
+  const dateInput = modal.querySelector("#event-date");
+  dateInput.addEventListener("focus", () => {
+    const pos = Math.max(0, dateInput.value.lastIndexOf("-"));
+    setTimeout(() => {
+      dateInput.setSelectionRange(pos, pos);
+    }, 1);
+  });
 }
