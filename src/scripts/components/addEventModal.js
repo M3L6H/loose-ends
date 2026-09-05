fb3c530e9110b252690ed5ac48f5b64c1920611a
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-09-04 23:34:34 -0500

    feat: add smart date completion

--- src/scripts/components/addEventModal.js
+++ src/scripts/components/addEventModal.js
@@ -1,11 +1,136 @@
 import { getTimeZone } from "../settings/index.js";
 
+// [T] -> 2026-09-04T
+// 2 [-] -> 2002-
+// 19 [-] -> 2019-
+// 134 [-] -> 2134-
+// 201 [0] -> 2010-
+// 2016 [-] -> 2016-
+// 2045 [1] -> 2045-1
+// 2003 [3] -> 2003-03-
+// 2026 [-] -> 2026-09-
+// 2712- [0] -> 2712-0
+// 2077- [8] -> 2077-08-
+// 2026- [-] -> 2026-09-
+// 2022-0 [5] -> 2022-05-
+// 2017-1 [2] -> 2017-12-
+// 2034-1 [-] -> 2034-01-
+// 2019-1 [3] -> 2019-01-3
+const yr = (i) => (c, v) => {
+  if (/[0-9]/.test(c)) {
+    v.push(c);
+    return true;
+  }
+  v.insert(0, getYear()[0]);
+  for (let j = i; i < 3; ++j) {
+    v.insert(0, "0");
+  }
+  return false;
+};
+const insZero = (c, v) => {
+  if (/[0-9]/.test(c)) {
+    v.push(c);
+    return true;
+  }
+
+  v.insert("0");
+  return false;
+};
+const upTo = (n, altFn) => (c, v) => {
+  if (/[0-9]/.test(c)) {
+    if (parseInt(c) <= n) {
+      v.push(c);
+      return true;
+    }
+
+    v.insert(0, "0");
+    v.push(c);
+    return true;
+  }
+
+  altFn().split("").forEach(ch => v.push(ch));
+  return false;
+};
+const sep = (s) => (c, v) => {
+  v.push(s);
+  return s === c;
+};
+const DATE_PARSERS = [
+  (c, v) => {
+    if (/[0-9]/.test(c)) {
+      v.push(c);
+      return true;
+    }
+    getYear().split("").forEach(ch => v.push(ch));
+    return false;
+  },
+  yr(1),
+  yr(2),
+  yr(3),
+  sep("-"),
+  upTo(1, getMonth),
+  insZero,
+  sep("-"), 
+  upTo(3, getDay),
+  insZero,
+  sep("T"),
+  upTo(2, getHour),
+  insZero,
+  sep(":"),
+  upTo(5, getMinute),
+  insZero,
+  sep(":"),
+  upTo(5, getSecond),
+  insZero
+];
+const MO_START_IDX = 5;
+
+function getNow() {
+  return Temporal.Now.zonedDateTimeISO(getTimeZone());
+}
+
+function getSecond(d) {
+  return (d ?? getNow()).toLocaleString("en-US", { second: "2-digit" })
+}
+
+function getMinute(d) {
+  return (d ?? getNow()).toLocaleString("en-US", { minute: "2-digit" })
+}
+
+function getHour(d) {
+  return (d ?? getNow()).toLocaleString("en-US", { hour: "2-digit" })
+}
+
+function getDay(d) {
+  return (d ?? getNow()).toLocaleString("en-US", { day: "2-digit" })
+}
+
+function getMonth(d) {
+  return (d ?? getNow()).toLocaleString("en-US", { month: "2-digit" })
+}
+
+function getYear(d) {
+  return (d ?? getNow()).toLocaleString("en-US", { year: "numeric" })
+}
+
+function backFillY(val, c) {
+  let fill = val ?? "";
+  const year = `${getNow().year}`;
+  if (fill.length === 0) {
+    return year;
+  }
+  if (fill.length === 1) {
+    fill = "0" + fill;
+  }
+  return year.substring(0, year.length - fill.length) + fill;
+}
+
 export function init() {
   const modal = document.getElementById("add-event-modal");
 
   modal.addEventListener("open", () => {
     const dateInput = modal.querySelector("#event-date");
-    const now = Temporal.Now.zonedDateTimeISO(getTimeZone()).toString({ 
+    const now = getNow().toString({ 
       timeZoneName: 'never', 
       fractionalSecondDigits: 0 
     });
@@ -19,5 +144,34 @@ export function init() {
     setTimeout(() => {
       dateInput.setSelectionRange(pos, pos);
     }, 1);
+    dateInput.dataset.prev = dateInput.value;
+  });
+
+  dateInput.addEventListener("beforeinput", (e) => {
+    if (e.inputType !== "insertText") {
+      return;
+    }
+
+    e.preventDefault();
+    const prev = dateInput.dataset.prev ?? "";
+    const text = e.data;
+    const pos = dateInput.selectionStart;
+    const end = pos + text.length;
+    let val = [];
+
+    for (let i = 0; i < end; ++i) {
+      const c = i < pos ? prev[i] : text[i - pos];
+      const parser = DATE_PARSERS[val.length];
+      if (!parser(c, val)) --i;
+    }
+    
+    val = val.join("") + prev.substring(pos);
+    
+    dateInput.value = val;
+    daysInput.dataset.prev = val;
+  });
+  
+  dateInput.addEventListener("input", (e) => {
+    dateInput.dataset.prev = e.target.value;
   });
 }
